271f5829526e2632a3e22061a9e39c42cc26f65f
김문현 <moo_nhe@naver.com>
2018-06-04 10:54:39 +0900

    jaja

--- hello.c
+++ hello.c
@@ -4,7 +4,7 @@ int main(void){
 	printf("hello");
 	printf("plus hello");
 	printf("moonhe modify");
-	printf("test modify");
+	printf("jaja modify");
 	printf("merge status");
 	printf("modify from git");
 	printf("modify from Github");
